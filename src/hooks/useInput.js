48d2e1527d489d42d588e70d5e0826960be1c95a
b4-you@lenovo <bayu.kdp@gmail.com>
2025-05-18 09:59:44 +0700

    make test error

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function useInput(defaultValue = '') {
   const [value, setValue] = React.useState(defaultValue);
-  const handleValueChange = (event) => setValue(event.target.value);
+  // const handleValueChange = (event) => setValue(event.target.value);
 
-  return [value, handleValueChange, setValue];
+  return [value, setValue];
 }
